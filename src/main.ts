af10e63135615122c63b424013fbd2ac2939d0e8
Sol-苏晨阳 <sol@fanruan.com>
2023-10-21 15:32:47 +0800

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express/adapters/express-adapter';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 
-const localURL = 'http://localhost:3000';
-const vercelURL = 'https://speed-1-frontend-depo55qng-notreallybenjamins-projects.vercel.app';
+// const localURL = 'http://localhost:3000';
+const vercelURL = 'https://speed-1-frontend-eight.vercel.app/';
 
   const CORS_OPTIONS = {
-    origin: localURL, // Replace with the actual origin of your frontend
+    origin: vercelURL, // Replace with the actual origin of your frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue : false,
     credentials: true,
@@ -17,7 +17,7 @@ const vercelURL = 'https://speed-1-frontend-depo55qng-notreallybenjamins-project
   async function bootstrap() {
     const app = await NestFactory.create(AppModule, new ExpressAdapter());
     app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
-    // app.enableCors(/*CORS_OPTIONS*/);
+    app.enableCors(CORS_OPTIONS);
 
     await app.listen(3001); // You can specify the port you want to listen on
 
